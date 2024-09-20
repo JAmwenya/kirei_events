64cf3baec64e2f30c7a83e83f051a06112497e87
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-09-20 18:21:25 +0300

    creating the database

---
+++ lib/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./music_album.db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def init_db():
+    # Importing the models
+    from lib.models.artist import Artist
+    from lib.models.album import Album
+
+    Base.metadata.create_all(bind=engine)
